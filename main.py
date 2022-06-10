041f913057825d4f01b986f9c77618256767a1a6
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-10 19:21:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+feeling="I hear you're feeling tired, get some rest bud"
+
+def name():
+  name = input("What is your name? ")
+  print("Hey", name, feeling)
+  print("Don't stay up on your phone!")
+  retort = input("Tell me why you should. ")
+  print("Well don't!")
+
+def main():
+  print("my value is")
+  print(1+2)
+  name()
+
+
+
+main()
